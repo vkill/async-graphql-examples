6f15006b93e49446018676ddd20633663780282e
vkill <vkill.net@gmail.com>
2020-05-16 23:47:46 +0800

    tide: add subscription example

---
+++ tide/subscription/src/main.rs
@@ -0,0 +1,174 @@
+use async_graphql::http::playground_source;
+use async_graphql::{Schema, WebSocketTransport};
+use async_std::net::{TcpListener, TcpStream};
+use async_std::task;
+use async_tungstenite::tungstenite::Message;
+use books::{MutationRoot, QueryRoot, Storage, SubscriptionRoot};
+use bytes::Bytes;
+use futures::select;
+use futures::{SinkExt, StreamExt};
+use std::env;
+use tide::{
+    http::{headers, mime},
+    Request, Response, StatusCode,
+};
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+type GQLSchema = Schema<QueryRoot, MutationRoot, SubscriptionRoot>;
+
+#[derive(Clone)]
+struct AppState {
+    schema: GQLSchema,
+}
+
+fn main() -> Result<()> {
+    task::block_on(run())
+}
+
+async fn run() -> Result<()> {
+    let schema = Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
+        .data(Storage::default())
+        .finish();
+    let schema_for_ws = schema.clone();
+
+    let app_state = AppState { schema };
+    let mut app = tide::with_state(app_state);
+
+    async fn graphql(req: Request<AppState>) -> tide::Result<Response> {
+        let schema = req.state().schema.clone();
+        async_graphql_tide::graphql(req, schema, |query_builder| query_builder).await
+    }
+
+    app.at("/graphql").post(graphql).get(graphql);
+    app.at("/").get(|_| async move {
+        let resp = Response::new(StatusCode::Ok)
+            .body_string(playground_source("/graphql", None))
+            .set_header(headers::CONTENT_TYPE, mime::HTML.to_string());
+
+        Ok(resp)
+    });
+
+    task::spawn(async move { run_ws(schema_for_ws).await });
+
+    let listen_addr = env::var("LISTEN_ADDR").unwrap_or_else(|_| "localhost:8000".to_owned());
+    println!("Playground: http://{}", listen_addr);
+    app.listen(listen_addr).await?;
+
+    Ok(())
+}
+
+async fn run_ws(schema: GQLSchema) -> Result<()> {
+    let listen_addr = env::var("WS_LISTEN_ADDR").unwrap_or_else(|_| "localhost:8001".to_owned());
+    let listener = TcpListener::bind(&listen_addr).await?;
+
+    while let Ok((stream, _)) = listener.accept().await {
+        task::spawn(accept_ws(stream, schema.clone()));
+    }
+
+    Ok(())
+}
+
+async fn accept_ws(stream: TcpStream, schema: GQLSchema) {
+    let ws_stream = async_tungstenite::accept_async(stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+
+    let (mut tx, rx) = ws_stream.split();
+    let (mut stx, srx) = schema
+        .clone()
+        .subscription_connection(WebSocketTransport::default());
+
+    let mut rx = rx.fuse();
+    let mut srx = srx.fuse();
+
+    loop {
+        select! {
+            bytes = srx.next() => {
+                if let Some(bytes) = bytes {
+                    if let Ok(text) = String::from_utf8(bytes.to_vec()) {
+                        if tx.send(Message::Text(text)).await.is_err()
+                        {
+                            return;
+                        }
+                    }
+                } else {
+                    return;
+                }
+            }
+            msg = rx.next() => {
+                if let Some(Ok(msg)) = msg {
+                    if msg.is_text() {
+                        if stx.send(Bytes::copy_from_slice(&msg.into_data())).await.is_err() {
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use async_std::prelude::*;
+    use serde_json::json;
+    use std::time::Duration;
+
+    #[test]
+    fn sample() -> Result<()> {
+        task::block_on(async {
+            let listen_addr = find_listen_addr().await;
+            env::set_var("LISTEN_ADDR", format!("{}", listen_addr));
+            let ws_listen_addr = find_listen_addr().await;
+            env::set_var("WS_LISTEN_ADDR", format!("{}", ws_listen_addr));
+
+            let server: task::JoinHandle<Result<()>> = task::spawn(async move {
+                run().await?;
+
+                Ok(())
+            });
+
+            let ws_client: task::JoinHandle<Result<()>> = task::spawn(async move {
+                let ws_listen_addr = env::var("WS_LISTEN_ADDR").unwrap();
+
+                task::sleep(Duration::from_millis(300)).await;
+
+                // TODO
+
+                Ok(())
+            });
+
+            let client: task::JoinHandle<Result<()>> = task::spawn(async move {
+                let listen_addr = env::var("LISTEN_ADDR").unwrap();
+
+                task::sleep(Duration::from_millis(300)).await;
+
+                let string = surf::post(format!("http://{}/graphql", listen_addr))
+                    .body_string(
+                        r#"{"query":"mutation { createBook(name: \"name1\", author: \"author1\") }"}"#
+                            .to_owned(),
+                    )
+                    .set_header("Content-Type".parse().unwrap(), "application/json")
+                    .recv_string()
+                    .await?;
+
+                println!("{}", string);
+                assert_eq!(string, json!({"data":{"createBook": "0"}}).to_string());
+
+                Ok(())
+            });
+
+            server.race(client).await?;
+
+            Ok(())
+        })
+    }
+
+    async fn find_listen_addr() -> async_std::net::SocketAddr {
+        async_std::net::TcpListener::bind("localhost:0")
+            .await
+            .unwrap()
+            .local_addr()
+            .unwrap()
+    }
+}
